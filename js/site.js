0863f9d23f94103d399252940e39d6ce499e0147
Martin Kool <martin@q42.nl>
2025-09-13 15:05:46 +0200

    Added logo's and prettified js/css/html

--- js/site.js
+++ js/site.js
@@ -1,17 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-	document.addEventListener('scroll', scroll);
-	scroll();
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("scroll", scroll);
+  scroll();
 
-	function scroll() {
-		let hasHash = document.location.href.indexOf('#') != -1;
-		let t = document.documentElement.scrollTop;
-		if (hasHash || t > window.innerHeight / 4) {
-			let hasClass = document.documentElement.className.indexOf('show-menu') != -1;
-			if (!hasClass) {
-				document.documentElement.className += ' show-menu';
-			}
-		} else {
-			document.documentElement.className = document.documentElement.className.replace(/ show-menu/gi, '');
-		}
-	}
+  function scroll() {
+    let hasHash = document.location.href.indexOf("#") != -1;
+    let t = document.documentElement.scrollTop;
+    if (hasHash || t > window.innerHeight / 4) {
+      let hasClass =
+        document.documentElement.className.indexOf("show-menu") != -1;
+      if (!hasClass) {
+        document.documentElement.className += " show-menu";
+      }
+    } else {
+      document.documentElement.className =
+        document.documentElement.className.replace(/ show-menu/gi, "");
+    }
+  }
 });
